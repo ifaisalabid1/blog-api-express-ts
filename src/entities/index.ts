d2803618d85233273f4894f0dd1f253a26ccfbb1
Faisal Abid <ifaisalabid1@gmail.com>
2025-10-08 23:45:01 +0530

    Add post and user entities

---
+++ src/entities/index.ts
@@ -0,0 +1,47 @@
+import { relations } from "drizzle-orm";
+import {
+  pgTable,
+  integer,
+  varchar,
+  boolean,
+  timestamp,
+  pgEnum,
+  text,
+} from "drizzle-orm/pg-core";
+
+export const rolesEnum = pgEnum("roles", ["user", "admin"]);
+
+export const users = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  role: rolesEnum().default("user").notNull(),
+  isActive: boolean("is_active").default(true).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  posts: many(posts),
+}));
+
+export const posts = pgTable("posts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 1000 }).notNull(),
+  content: text().notNull(),
+  excerpt: text(),
+  slug: varchar({ length: 500 }).notNull(),
+  isPublished: boolean("is_published").default(false).notNull(),
+  authorId: integer("author_id").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  publishedAt: timestamp("published_at"),
+});
+
+export const postsRelations = relations(posts, ({ one }) => ({
+  author: one(users, {
+    fields: [posts.authorId],
+    references: [users.id],
+  }),
+}));
